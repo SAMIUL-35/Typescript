cfb80a53cb5ec5bef33b2203fe4e2669fa3e6235
SAMIUL2022 <sami35bd@gmail.com>
2024-11-01 22:41:47 +0600

    generic Type

--- module-2/src/3.3.ts
+++ module-2/src/3.3.ts
@@ -1,12 +1,29 @@
 {
 
+// generic type
+
+type GenericArray<T> =Array<T>
+
+// const rollNumbers:number[]=[1,2,8]
+// const mentors:string[]=['m','v']
+// const boolArry:boolean[]=[true,false]
+const rollNumbers:GenericArray<number>=[1,2,8]
+const mentors:GenericArray<string>=['m','v']
+const boolArry:GenericArray<boolean>=[true,false]
+
+const user:GenericArray<{name:string,age:number}>=[
+  {name:'samiul',
+    age:200
+  },
+  {name:'nabil',
+    age:200
+  }
+]
+type GenericTupple<x,y>=[x,y]
+const manus:GenericTupple<string,string>=['me','he']
+
+const userWithid:GenericTupple<number,{name:string}=[1234,{name:'samiul',}]
 
 
 
-
-
-
-
-
-  
 }
